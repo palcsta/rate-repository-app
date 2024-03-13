d55a9540c800ad7a534d3d4a1767a8bb588d428d
palcsta <palchyk@github.com>
2024-03-13 15:42:39 +0200

    10.1

---
+++ App.js
@@ -0,0 +1,25 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import { Button } from 'react-native-web';
+
+export default function App() {
+  //create something cool looking for an phone app
+  return (<>
+    <View style={styles.container}>
+      <Text style={styles.title}>Welcome to Rate Repository App</Text>
+      <Text style={styles.subtitle}>Discover and rate GitHub repositories</Text>
+      
+      <StatusBar style="auto" />
+    </View>
+    </>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
