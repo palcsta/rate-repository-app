1aca50ff237840727cb43fd486c5a3231486fde7
palcsta <palchyk@github.com>
2024-03-14 17:57:56 +0200

    10.2

--- App.js
+++ App.js
@@ -1,15 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { Button } from 'react-native-web';
+import React, { useState } from 'react';
 
 export default function App() {
   //create something cool looking for an phone app
+  const [value, setValue] = useState(0);
   return (<>
     <View style={styles.container}>
       <Text style={styles.title}>Welcome to Rate Repository App</Text>
       <Text style={styles.subtitle}>Rate GitHub repositories</Text>
-      <Button title="Sign in" onPress={() => alert('Sign in button pressed')} />
-      
+      <Text style={styles.subtitle}>{value}</Text>
+      <Button title="console.logs" onPress={() => setValue(value+1)}/>
       <StatusBar style="auto" />
     </View>
     </>
@@ -19,7 +21,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'red',
     alignItems: 'center',
     justifyContent: 'center',
   },
